f251436bd3c631baa536c3b7640b35d6b3b05089
James Kenny <james.m.kenny@gmail.com>
2016-11-10 16:46:00 +0000

    Write a message to the queue

--- SQS_Write_Message_Console/Program.cs
+++ SQS_Write_Message_Console/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Amazon.SQS;
+using Amazon.SQS.Model;
 
 namespace SQS_Write_Message_Console
 {
@@ -10,6 +12,34 @@ namespace SQS_Write_Message_Console
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting..");
+
+            string sQueue = "https://sqs.eu-west-1.amazonaws.com/{CODE}/{QUEUENAME}"; // Region / CODE and QueueName
+
+            try
+            {
+                var config = new AmazonSQSConfig()
+                {
+                    ServiceURL = "https://sqs.eu-west-1.amazonaws.com/" // Region and URL
+                };
+
+                var _messageRequest = new SendMessageRequest();
+
+                _messageRequest.QueueUrl = sQueue;
+
+                _messageRequest.MessageBody = "This is a test message";
+
+                AmazonSQSClient _client = new AmazonSQSClient("ACCESSKEY", "ACCESSSECRET", config);
+
+                SendMessageResponse sendMessageResponse = _client.SendMessage(_messageRequest);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.ToString());
+            }
+
+            Console.WriteLine("Complete");
         }
     }
 }
